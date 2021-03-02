1ad8552822c50f2f44c10ad798ae2aec69c75791
AlexandrLyachov1 <72990929+AlexandrLyachov1@users.noreply.github.com>
2021-03-02 09:51:54 +0300

    add code for espnow

---
+++ esp_now/esp_now.ino
@@ -0,0 +1,59 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+
+typedef struct struct_message {
+  char a[32];
+  int b;
+  float c;
+  String d;
+  bool e;
+} struct_message;
+
+struct_message myData;
+
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 2000;  
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+ void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+ 
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+  
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+ 
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    strcpy(myData.a, "THIS IS A CHAR");
+    myData.b = random(1,20);
+    myData.c = 1.2;
+    myData.d = "Hello";
+    myData.e = false;
+
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+    lastTime = millis();
+  }
+}
